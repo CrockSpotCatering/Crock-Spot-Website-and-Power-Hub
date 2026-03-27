98723026e23251b933845992e20f6be327bf30ef
CrockSpotCatering <CrockSpotCatering@users.noreply.github.com>
2026-03-27 06:59:07 -0600

    Center the 3 partnership type cards in How We Partner section

--- app/community-partners/page.tsx
+++ app/community-partners/page.tsx
@@ -99,7 +99,7 @@ export default function CommunityPartners() {
             </p>
           </motion.div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-4xl mx-auto">
             {partnershipTypes.types.map((item, index) => (
               <motion.div
                 key={index}
